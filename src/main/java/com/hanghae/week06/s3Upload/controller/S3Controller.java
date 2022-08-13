19b18db20a334a08cb5892355beb26dcdeafe9ae
Paul Kim <kms30423@gmail.com>
2022-08-13 15:47:02 +0900

    feat:image upload기능 구현

---
+++ src/main/java/com/hanghae/week06/s3Upload/controller/S3Controller.java
@@ -0,0 +1,26 @@
+package com.hanghae.week06.s3Upload.controller;
+
+import com.hanghae.week06.s3Upload.service.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class S3Controller {
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/upload")
+    public String updateUserImage(@RequestParam(value = "file") MultipartFile multipartFile) {
+        String result = "";
+        try {
+            result = s3Uploader.uploadFiles(multipartFile, "static");
+        } catch (Exception e) {
+            return "업로드 에러 발생";
+        }
+        return result;
+    }
+
+}
